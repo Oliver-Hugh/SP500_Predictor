d6b5e9368fa38e9c62a6e1172a5af6e32d0f63f9
oliverhugh <oliver.hugh.1073@gmail.com>
2023-12-12 19:35:37 -0500

    LSTM Algorithm Working, with metrics

--- lstm_oliver.py
+++ lstm_oliver.py
@@ -2,24 +2,25 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
-import yfinance
-import financial_data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import layers
+
+window_size = 5
 
 
-def str_to_datetime(s):
-    #s = str(s)
-    #s = s[0:10]
+def string_to_datetime(s):
     split = s.split('-')
-    year, month, day = int(split[0]), int(split[1]), int(split[2])
+    year = int(split[0])
+    month = int(split[1])
+    day = int(split[2])
     return datetime.datetime(year=year, month=month, day=day)
 
 
-def df_to_windowed_df(dataframe, first_date_str, last_date_str, n=3):
-    first_date = str_to_datetime(first_date_str)
-    last_date = str_to_datetime(last_date_str)
+def df_to_windowed_df(dataframe, first_date_str, last_date_str, n=window_size):
+    first_date = string_to_datetime(first_date_str)
+    last_date = string_to_datetime(last_date_str)
     target_date = first_date
-    #print(target_date)
-    #print(type(target_date))
     dates = []
     X, Y = [], []
     last_time = False
@@ -48,91 +49,208 @@ def df_to_windowed_df(dataframe, first_date_str, last_date_str, n=3):
     ret_df = pd.DataFrame({})
     ret_df['Target Date'] = dates
     X = np.array(X)
-    for i in range(0, n):
-        X[:, i]
-        ret_df[f'Target-{n - i}'] = X[:, i]
+    for j in range(n):
+        X[:, j]
+        ret_df[f'Target-{n - j}'] = X[:, j]
     ret_df['Target'] = Y
     return ret_df
 
 
 def windowed_df_to_date_X_y(windowed_dataframe):
     df_as_np = windowed_dataframe.to_numpy()
-    dates = df_as_np[:, 0]
+    date_list = df_as_np[:, 0]
     middle_matrix = df_as_np[:, 1:-1]
-    X = middle_matrix.reshape((len(dates), middle_matrix.shape[1], 1))
+    X = middle_matrix.reshape((len(date_list), middle_matrix.shape[1], 1))
     Y = df_as_np[:, -1]
-    return dates, X.astype(np.float32), Y.astype(np.float32)
+    return date_list, X.astype(np.float32), Y.astype(np.float32)
 
 
 sp_df = pd.read_csv('^SPX.csv')
 sp_df = sp_df[['Date', 'Close']]
 close_data = sp_df['Close']
+dates = sp_df['Date']
 
-
-# start and end dates for all data (training, validation, and test)
-#start = "2000-01-01"
-#end = "2023-12-11"
-#imported_df = financial_data.get_timeframe_data(start, end, False)
-
-# The following code will take the data from the imported dataframe, and convert
-# it to a new dataframe that is more convenient to use
-#close_data = imported_df['Close']
 size = len(close_data)
 date_lst = []
-close_pc_lst = []
-last_close = close_data.iloc[0]
-first_close = last_close
-# Get the dates, as they are by default the index
+close_lst = []
 # Also get the close prices and put them into the close_lst
-"""for i in range(1, size):
-    #date_string = str(imported_df.index[i])
-    #date_string = str(sp_df.index[i])
-    #date_string = date_string[0:10]
-    #date_lst.append(date_string)
-    
+last_close = close_data.iloc[0]
+bias = last_close  # the first value
+for i in range(size):
+    date_string = str(dates.iloc[i])
+    date_string = date_string[0:10]
+    date_lst.append(date_string)
     close = close_data.iloc[i]
-    close_diff = close - last_close
-    close_percent_change = close_diff / first_close
-    close_pc_lst.append(close_percent_change)
-    last_close = close"""
+    close_diff = close - bias
+    close_percent_change = close_diff / bias
+    last_close = close
+    close_lst.append(close_percent_change)
 # This is our newly formatted dataframe
-#sp_df = pd.DataFrame(data={'Date': date_lst, 'Close': close_pc_lst})
+sp_df = pd.DataFrame(data={'Date': date_lst, 'Close': close_lst})
 # We want to format the dates from a string to datetime now, using our function
-sp_df['Date'] = sp_df['Date'].apply(str_to_datetime)
+sp_df['Date'] = sp_df['Date'].apply(string_to_datetime)
 sp_df.index = sp_df.pop('Date')
-#plt.plot(sp_df.index, sp_df['Close'])
-#plt.show()
+print(sp_df)
 
 # Start day second time around: '2021-03-25'
-windowed_df = df_to_windowed_df(sp_df, '2021-03-25', '2022-03-23', n=3)
-#print(windowed_df)
+windowed_df = df_to_windowed_df(sp_df, '2019-01-01', '2023-12-01')
 dates, X, y = windowed_df_to_date_X_y(windowed_df)
-q_80 = int(len(dates) * .8)
-q_90 = int(len(dates) * .9)
-dates_train, X_train, y_train = dates[:q_80], X[:q_80], y[:q_80]
-dates_val, X_val, y_val = dates[q_80:q_90], X[q_80:q_90], y[q_80:q_90]
-dates_test, X_test, y_test = dates[q_90:], X[q_90:], y[q_90:]
-"""plt.plot(dates_train, y_train)
-plt.plot(dates_val, y_val)
-plt.plot(dates_test, y_test)
-plt.legend(['Train', 'Validation', 'Test'])
-#plt.show()"""
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras import layers
-model = Sequential([layers.Input((3, 1)),
-                    layers.LSTM(64),
+# Calculate values to partition data set between training, validation, and testing
+num_datapoints = int(len(dates))
+training_proportion = .75
+training_section = int(training_proportion * num_datapoints)
+val_test_split = int(((1 - training_proportion) * .5 + training_proportion) * num_datapoints)
+
+dates_train, X_train, y_train = dates[:training_section], X[:training_section], y[:training_section]
+dates_val, X_val, y_val = (dates[training_section:val_test_split],
+                           X[training_section:val_test_split], y[training_section:val_test_split])
+dates_test, X_test, y_test = dates[val_test_split:], X[val_test_split:], y[val_test_split:]
+
+
+model = Sequential([layers.Input((window_size, 1)),
+                    layers.LSTM(50),
                     layers.Dense(32, activation='relu'),
                     layers.Dense(32, activation='relu'),
                     layers.Dense(1)])
 model.compile(loss='mse',
-              optimizer=Adam(learning_rate=0.0001),
+              optimizer=Adam(learning_rate=0.001),
               metrics=['mean_absolute_error'])
-model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=50)
+model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=200)
 train_predictions = model.predict(X_train).flatten()
+val_predictions = model.predict(X_val).flatten()
+test_predictions = model.predict(X_test).flatten()
 
-plt.plot(dates_train, train_predictions)
-plt.plot(dates_train, y_train)
-plt.legend(['Training Predictions', 'Training Observations'])
+
+# the following are the pseudo normalized values
+
+plt.plot(dates_train, y_train)  # Training Observations
+plt.plot(dates_val, y_val)      # Validation Observations
+plt.plot(dates_test, y_test)    # Test Observations
+
+plt.plot(dates_train, train_predictions)    # Training Predictions
+plt.plot(dates_val, val_predictions)        # Validation Predictions
+plt.plot(dates_test, test_predictions)      # Test Predictions
+plt.legend(['Training Observations', 'Validation Observations', 'Test Observations',
+            'Training Predictions', 'Validation Predictions', 'Test Predictions'])
+plt.xlabel('Time')
+plt.ylabel('(S&P 500 Closing Price - Bias)/Bias')
+plt.title('Pseudo-Normalized Closing Price Prediction vs. Time')
+plt.show()
+
+classification_error = 0
+
+train_predictions_actual = []
+y_train_actual = []
+train_error_lst = []
+train_error_sum = 0
+# for loop for training data
+for a in range(len(dates_train)):
+    # Reverse pseudo normalization
+    prediction_actual = train_predictions[a] * bias + bias
+    y_value = y_train[a] * bias + bias
+
+    # Append to empty lists
+    y_train_actual.append(y_value)
+    train_predictions_actual.append(prediction_actual)
+
+    # Compute error for metrics
+    train_error = (prediction_actual - y_value) / y_value
+    train_error_lst.append(train_error)
+    train_error_sum = train_error_sum + abs(train_error)
+    if a > 0:
+        y_diff = y_value - y_train_actual[a-1]
+        prediction_diff = prediction_actual - y_train_actual[a-1]
+        if (prediction_diff < 0 and y_diff > 0) or (prediction_diff > 0 and y_diff < 0):
+            classification_error += 1
+
+
+val_predictions_actual = []
+y_val_actual = []
+val_error_lst = []
+val_error_sum = 0
+# for loop for validation data
+for b in range(len(dates_val)):
+    # Reverse pseudo normalization
+    prediction_actual = val_predictions[b] * bias + bias
+    y_value = y_val[b] * bias + bias
+
+    # Append to empty lists
+    val_predictions_actual.append(prediction_actual)
+    y_val_actual.append(y_value)
+
+    # Compute error for metrics
+    val_error = (prediction_actual - y_value) / y_value
+    val_error_lst.append(val_error)
+    val_error_sum = val_error_sum + abs(val_error)
+    # Check for classification error
+    if b > 0:
+        y_diff = y_value - y_val_actual[b-1]
+        prediction_diff = prediction_actual - y_val_actual[b-1]
+        if (prediction_diff < 0 and y_diff > 0) or (prediction_diff > 0 and y_diff < 0):
+            classification_error += 1
+
+# for loop for test data
+test_predictions_actual = []
+y_test_actual = []
+test_error_lst = []
+test_error_sum = 0
+# for loop for validation data
+for c in range(len(dates_test)):
+    # Reverse pseudo normalization
+    prediction_actual = test_predictions[c] * bias + bias
+    y_value = y_test[c] * bias + bias
+
+    # Append to empty lists
+    test_predictions_actual.append(prediction_actual)
+    y_test_actual.append(y_value)
+
+    # Compute error for metrics
+    test_error = (prediction_actual - y_value) / y_value
+    test_error_lst.append(test_error)
+    test_error_sum = test_error_sum + abs(test_error)
+    # Check for classification error
+    if c > 0:
+        y_diff = y_value - y_test_actual[c-1]
+        prediction_diff = prediction_actual - y_test_actual[c-1]
+        if (prediction_diff < 0 and y_diff > 0) or (prediction_diff > 0 and y_diff < 0):
+            classification_error += 1
+
+
+# Plot the Actual stock value predictions
+plt.figure()
+plt.plot(dates_train, y_train_actual)  # Training Observations
+plt.plot(dates_val, y_val_actual)      # Validation Observations
+plt.plot(dates_test, y_test_actual)    # Test Observations
+
+plt.plot(dates_train, train_predictions_actual)    # Training Predictions
+plt.plot(dates_val, val_predictions_actual)        # Validation Predictions
+plt.plot(dates_test, test_predictions_actual)      # Test Predictions
+plt.legend(['Training Observations', 'Validation Observations', 'Test Observations',
+            'Training Predictions', 'Validation Predictions', 'Test Predictions'])
+plt.xlabel('Time')
+plt.ylabel('S&P 500 Closing Price')
+plt.title('Closing Price Prediction vs. Time')
+plt.show()
+
+avg_train_error = train_error_sum / len(train_error_lst)
+avg_val_error = val_error_sum / len(val_error_lst)
+avg_test_error = test_error_sum / len(test_error_lst)
+print("Average Training Error is ", avg_train_error)
+print("Average Validation Error is ", avg_val_error)
+print("Average Test Error is ", avg_test_error)
+
+print("Classification Errors: ", classification_error)
+total_classification_error = classification_error/len(dates)
+print("Total Classification Error is: ", total_classification_error)
+print("This is a classification Accuracy of ", (1 - total_classification_error))
+
+plt.legend(['Training Error', 'Validation Error', 'Test Error'])
+plt.figure()
+plt.plot(dates_train, train_error_lst)
+plt.plot(dates_val, val_error_lst)
+plt.plot(dates_test, test_error_lst)
+plt.ylabel("Error")
+plt.xlabel("Time")
+plt.title("Stock Price Observation Error")
 plt.show()
